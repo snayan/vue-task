abbb6e3601b1c3ab115ff991e9332d332030bac9
yang.zhang <yang.zhang@shopee.com>
2018-03-21 23:36:23 +0800

    repos init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import Vue from 'vue';
-import App from './App.vue';
+import App from './views/App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import './scss/reset.scss';
 
 Vue.config.productionTip = false;
 
